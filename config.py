c8104678b4701ce8893ada0389a6a732ab7c9f41
𝐍ᴀᴋs𝐇  𝐕ᴀɪ <166161356+Wizardtexh@users.noreply.github.com>
2024-04-26 16:40:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 API_ID = int(os.environ.get("API_ID",29109422 ))
 API_HASH = os.environ.get("API_HASH", None) 
 BOT_TOKEN = os.environ.get("7133714014:AAEyDU_CBLOimdqZrqB8jPJtO7JMP-seYCo") 
-OPENAI_API_KEY= os.environ.get("sk-proj-kXHvvW8eES71Y5dJiQQLT3BlbkFJh9YyslKLk5GMbZcBfAhb")
+'openai.api_key =sk-proj-kXHvvW8eES71Y5dJiQQLT3BlbkFJh9YyslKLk5GMbZcBfAhb ',
 BOT_USERNAME = os.environ.get("nakshchatgpt_bot") 
 SUDO=os.environ.get("SUDO","7080052423")
 BOT_NAME = os.environ.get("naxx")
